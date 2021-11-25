833bc4994510c822bee382dfcf95da575923c49b
unknown <mahi121.mr@gmail.com>
2021-11-25 10:58:58 +0600

    Header Footer Added

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-yellow-300 py-8 text-gray-600'>
+            Copyright © Mahinur Rahman 2021
+        </div>
+    );
+};
+
+export default Footer;
